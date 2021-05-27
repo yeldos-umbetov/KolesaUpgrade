1d830507bf01818dc3bf7c6e01f450f38160bfcd
yeldos-umbetov <yeleke.umbetov@gmail.com>
2021-05-27 09:37:28 +0600

    lesson 4-fix after comment

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -2,13 +2,10 @@
 
 use Codeception\Util\ActionSequence;
 
-class FirstCest
+class QuickViewCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
 
-    // tests
+    // Тест на проверку работы модального окна при нажатии на Quick View
     public function quickViewTitle(AcceptanceTester $I)
     {
         $I->amOnPage('');
@@ -16,8 +13,7 @@ class FirstCest
         $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
         $I->seeElement('#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-mobile-line.hovered > div > div.left-block > div > a.quick-view');
         $I->click('#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-mobile-line.hovered > div > div.left-block > div > a.quick-view');
-        $I->performOn('.product', ActionSequence::build()
-            ->seeElement('.#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1')
-    );
+        $I->switchToIFrame('.fancybox-iframe');
+        $I->see('Blouse', '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
     }
 }
